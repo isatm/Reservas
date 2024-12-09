fb8a1e79ec754786c8516bac4a933fb87e842450
Jerónimo RD <jeronimoramirezd24@gmail.com>
2024-12-09 10:50:27 -0500

    Se creo inputs de createEvent y Event

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EventComponent } from './pages/event/event.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'createEvent', component: CreateEventComponent}
+    {path: 'createEvent', component: CreateEventComponent},
+    {path: 'event', component: EventComponent}
 ];
