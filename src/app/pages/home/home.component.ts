df1bdbf822df5da3d06346114a4112baf220a9b3
Jerónimo RD <jeronimoramirezd24@gmail.com>
2024-12-11 11:12:11 -0500

    Se puede filtrar por nombre

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CarouselComponent } from '../../components/carousel/carousel.component';
-import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { EventoService } from '../../services/evento.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,25 +8,39 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, SearchBarComponent,HttpClientModule,FormsModule,CommonModule,RouterModule],
+  imports: [HttpClientModule,FormsModule,CommonModule,RouterModule],
   providers: [EventoService], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   evento: any[] = [];
+  eventosFiltrados: any[] = [];
+  busqueda: string = ''; // Variable para almacenar la búsqueda
 
   constructor(private EventoService: EventoService) {}
 
   ngOnInit(): void {
-    this.EventoService.obtenerEvento().subscribe({
+    this.EventoService.obtenerEvento(false).subscribe({
       next: (data) => {
         console.log(data);
-        this.evento = data; // Guardar los datos en la variable
+        this.evento = data; // Guardar todos los eventos
+        this.eventosFiltrados = data; // Inicializar la lista filtrada
       },
       error: (err) => {
         console.error('Error fetching events:', err);
       },
     });
+  }
+
+  filtrarEventos(event: Event): void {
+    event.preventDefault(); // Evita el comportamiento por defecto del formulario
+    const busquedaLower = this.busqueda.toLowerCase();
+
+    // Filtrar eventos según el término de búsqueda
+    this.eventosFiltrados = this.evento.filter((e) =>
+      e.eve_nombre.toLowerCase().includes(busquedaLower) ||
+      e.eve_descripcion.toLowerCase().includes(busquedaLower)
+    );
   }
 }
